6b5c2f023c6f661c1e335ff52023448f4656733a
Chz187 <139023608+Chz187@users.noreply.github.com>
2023-07-09 03:23:17 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,30 +1,11 @@
 import streamlit as st
-from streamlit_chat import message
-from streamlit_extras.colored_header import colored_header
-from streamlit_extras.add_vertical_space import add_vertical_space
 from hugchat import hugchat
 from hugchat.login import Login
 
-# Log in to huggingface and grant authorization to huggingchat
-sign = Login(email, passwd)
-cookies = sign.login()
+# App title
+st.set_page_config(page_title="🤗💬 HugChat")
 
-# Save cookies to usercookies/<email>.json
-sign.saveCookies()
-
-# Create a ChatBot
-chatbot = hugchat.ChatBot(cookies=cookies.get_dict())  # or cookie_path="usercookies/<email>.json"
-print(chatbot.chat("HI"))
-
-# Create a new conversation
-id = chatbot.new_conversation()
-chatbot.change_conversation(id)
-
-# Get conversation list
-conversation_list = chatbot.get_conversation_list()
-
-st.set_page_config(page_title="ChzChat - An LLM-powered AI assistant app")
-
+# Hugging Face Credentials
 with st.sidebar:
     st.title('🤗💬 HugChat')
     if ('EMAIL' in st.secrets) and ('PASS' in st.secrets):
@@ -38,43 +19,39 @@ with st.sidebar:
             st.warning('Please enter your credentials!', icon='⚠️')
         else:
             st.success('Proceed to entering your prompt message!', icon='👉')
+
     
+# Store LLM generated responses
+if "messages" not in st.session_state.keys():
+    st.session_state.messages = [{"role": "assistant", "content": "How may I help you?"}]
 
-if 'generated' not in st.session_state:
-    st.session_state['generated'] = ["I'm Juan, your AI assistant, How may I help you?"]
+# Display chat messages
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.write(message["content"])
 
-if 'past' not in st.session_state:
-    st.session_state['past'] = ['Hi!']
+# Function for generating LLM response
+def generate_response(prompt_input, email, passwd):
+    # Hugging Face Login
+    sign = Login(email, passwd)
+    cookies = sign.login()
+    #sign.saveCookies()
+    # Create ChatBot                        
+    chatbot = hugchat.ChatBot(cookies=cookies.get_dict())
+    return chatbot.chat(prompt_input)
 
-input_container = st.container()
-colored_header(label='', description='', color_name='blue-30')
-response_container = st.container()
+# User-provided prompt
+if prompt := st.chat_input(disabled=not (hf_email and hf_pass)):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.write(prompt)
 
-# User input
-## Function for taking user provided prompt as input
-def get_text():
-    input_text = st.text_input("You: ", "", key="input")
-    return input_text
-
-## Applying the user input box
-with input_container:
-    user_input = get_text()
-
-# Response output
-## Function for taking user prompt as input followed by producing AI generated responses
-def generate_response(prompt):
-    chatbot = hugchat.ChatBot()
-    response = chatbot.chat(prompt)
-    return response
-
-## Conditional display of AI generated responses as a function of user provided prompts
-with response_container:
-    if user_input:
-        response = generate_response(user_input)
-        st.session_state.past.append(user_input)
-        st.session_state.generated.append(response)
-        
-    if st.session_state['generated']:
-        for i in range(len(st.session_state['generated'])):
-            message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
-            message(st.session_state['generated'][i], key=str(i))
+# Generate a new response if last message is not from assistant
+if st.session_state.messages[-1]["role"] != "assistant":
+    #with st.chat_message("assistant"):
+    with st.chat_message("assistant"):
+        with st.spinner("Thinking..."):
+            response = generate_response(prompt, hf_email, hf_pass) 
+            st.write(response) 
+    message = {"role": "assistant", "content": response}
+    st.session_state.messages.append(message)
